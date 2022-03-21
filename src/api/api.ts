d66255a9140c7f7635bcd3b25e2e5b5b62e763d4
Romeu Franzoia Junior <rfranzoia@gmail.com>
2022-03-21 23:07:12 +0000

    added jwt token validation to all api

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,13 +4,14 @@ import productRouter from "./ProductsRouter";
 import productPricesRouter from "./ProductPricesRouter";
 import usersRouter from "./UsersRouter";
 import ordersRouter from "./OrdersRouter";
+import {TokenService} from "../security/TokenService";
 
 const api = Router();
 
-api.use("/productTypes", productTypeRouter);
-api.use("/products", productRouter);
-api.use("/productPrices", productPricesRouter);
 api.use("/users", usersRouter);
-api.use("/orders", ordersRouter);
+api.use("/productTypes", TokenService.getInstance().authenticateToken, productTypeRouter);
+api.use("/products", TokenService.getInstance().authenticateToken, productRouter);
+api.use("/productPrices", TokenService.getInstance().authenticateToken, productPricesRouter);
+api.use("/orders", TokenService.getInstance().authenticateToken, ordersRouter);
 
 export default api;
