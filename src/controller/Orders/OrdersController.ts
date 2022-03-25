6d97ea8d3dce3d231189b8d51c09b4fa9f5835d4
Romeu Franzoia Junior <rfranzoia@gmail.com>
2022-03-25 23:20:17 +0000

    changed OrdersService to not return ResponseData and moved that to OrdersController

--- src/controller/Orders/OrdersController.ts
+++ src/controller/Orders/OrdersController.ts
@@ -1,6 +1,12 @@
 import {Request, Response} from "express";
 import {OrdersService} from "../../service/Orders/OrdersService";
 import {PaginationService} from "../../service/PaginationService";
+import {StatusCodes} from "http-status-codes";
+import {ResponseData} from "../ResponseData";
+import NotFoundError from "../../utils/errors/NotFoundError";
+import InternalServerError from "../../utils/errors/InternalServerError";
+import BadRequestError from "../../utils/errors/BadRequestError";
+import {OrderDTO} from "./OrderDTO";
 
 export class OrdersController {
 
@@ -15,64 +21,90 @@ export class OrdersController {
 
     async count(request: Request, response: Response) {
         const result = await OrdersController.getService().count();
-        response.status(result.statusCode).send(result);
+        return response.status(StatusCodes.OK).send({ count: result});
     }
 
     async list(request: Request, response: Response) {
         const {skip, limit} = await PaginationService.getInstance().getPagination(request.query);
         const userId = Number(request["user"].id);
-        const result = await OrdersController.getService().list(skip, limit, userId);
-        response.status(result.statusCode).send(result);
+        try {
+            const result = await OrdersController.getService().list(skip, limit, userId);
+            return response.status(StatusCodes.OK).send(new ResponseData(StatusCodes.OK, "", result));
+        } catch (error) {
+            return response.status(StatusCodes.INTERNAL_SERVER_ERROR).send(new ResponseData(StatusCodes.INTERNAL_SERVER_ERROR, error.message, error));
+        }
     }
 
     async listByDateRange(request: Request, response: Response) {
         const {skip, limit} = await PaginationService.getInstance().getPagination(request.query);
         const {start_date, end_date} = request.params;
         const userId = Number(request["user"].id);
-        const result = await OrdersController.getService().listByDateRange(new Date(start_date), new Date(end_date), skip, limit, userId);
-        response.status(result.statusCode).send(result);
+        try {
+            const result = await OrdersController.getService().listByDateRange(new Date(start_date), new Date(end_date), skip, limit, userId);
+            return response.status(StatusCodes.OK).send(new ResponseData(StatusCodes.OK, "", result));
+        } catch (error) {
+            return response.status(StatusCodes.INTERNAL_SERVER_ERROR).send(new ResponseData(StatusCodes.INTERNAL_SERVER_ERROR, error.message, error));
+        }
     }
 
     async listByStatus(request: Request, response: Response) {
         const {skip, limit} = await PaginationService.getInstance().getPagination(request.query);
         const {order_status} = request.params;
         const userId = Number(request["user"].id);
-        const result = await OrdersController.getService().listByStatus(order_status, skip, limit, userId);
-        response.status(result.statusCode).send(result);
+        try {
+            const result = await OrdersController.getService().listByStatus(order_status, skip, limit, userId);
+            return response.status(StatusCodes.OK).send(new ResponseData(StatusCodes.OK, "", result));
+        } catch (error) {
+            return response.status(StatusCodes.INTERNAL_SERVER_ERROR).send(new ResponseData(StatusCodes.INTERNAL_SERVER_ERROR, error.message, error));
+        }
     }
 
     async listOrderHistoryByOrderId(request: Request, response: Response) {
         const {skip, limit} = await PaginationService.getInstance().getPagination(request.query);
         const { id } = request.params;
         const userId = Number(request["user"].id);
-        const result = await OrdersController.getService().listOrderHistoryByOrderId(id, skip, limit, userId);
-        response.status(result.statusCode).send(result);
+        try {
+            const result = await OrdersController.getService().listOrderHistoryByOrderId(id, skip, limit, userId);
+            return response.status(StatusCodes.OK).send(new ResponseData(StatusCodes.OK, "", result));
+        } catch (error) {
+            return response.status(StatusCodes.INTERNAL_SERVER_ERROR).send(new ResponseData(StatusCodes.INTERNAL_SERVER_ERROR, error.message, error));
+        }
     }
 
     async get(request: Request, response: Response) {
         const { id } = request.params;
         const userId = Number(request["user"].id);
-        const result = await OrdersController.getService().get(id, userId);
-        response.status(result.statusCode).send(result);
+        try {
+            const result = await OrdersController.getService().get(id, userId);
+            if (result instanceof NotFoundError) {
+                return response.status(StatusCodes.NOT_FOUND).send(new ResponseData(StatusCodes.NOT_FOUND, "", result))
+            }
+            return response.status(StatusCodes.OK).send(new ResponseData(StatusCodes.OK, "", result));
+        } catch (error) {
+            return response.status(StatusCodes.INTERNAL_SERVER_ERROR).send(new ResponseData(StatusCodes.INTERNAL_SERVER_ERROR, error.message, error));
+        }
     }
 
     async create(request: Request, response: Response) {
         const { user_id, orderItems } = request.body;
         const result = await OrdersController.getService().create(user_id, orderItems);
-        response.status(result.statusCode).send(result);
+        if (result instanceof InternalServerError) {
+            return response.status(StatusCodes.INTERNAL_SERVER_ERROR).send(new ResponseData(StatusCodes.INTERNAL_SERVER_ERROR, "", result))
+        }
+        return response.status(StatusCodes.CREATED).send(new ResponseData(StatusCodes.CREATED, "", result));
     }
 
     async addRemoveOrderItems(request: Request, response: Response) {
         const { id } = request.params;
         const { orderItems } = request.body;
         const result = await OrdersController.getService().addRemoveOrderItems(id, orderItems);
-        response.status(result.statusCode).send(result);
+        return processResult(result, response, StatusCodes.OK, new ResponseData(StatusCodes.OK, "", result));
     }
 
     async delete(request: Request, response: Response) {
         const { id } = request.params;
         const result = await OrdersController.getService().deleteOrderAndItems(id);
-        response.status(result.statusCode).send(result);
+        return processResult(result, response, StatusCodes.NO_CONTENT, new ResponseData(StatusCodes.NO_CONTENT, "Pedido removido com sucesso"));
     }
 
     async updateStatus(request: Request, response: Response) {
@@ -80,6 +112,21 @@ export class OrdersController {
         const { orderStatus, changeReason } = request.body;
         const userId = Number(request["user"].id);
         const result = await OrdersController.getService().updateOrderStatus(id, orderStatus, changeReason, userId);
-        response.status(result.statusCode).send(result);
+        return processResult(result, response, StatusCodes.OK, new ResponseData(StatusCodes.OK, "Status do pedido atualizado com sucesso", result))
     }
+}
+
+const processResult = (result: void | OrderDTO | BadRequestError | NotFoundError | InternalServerError,
+                       response: Response,
+                       statusCode: number,
+                       responseData: ResponseData) => {
+    if (result instanceof BadRequestError) {
+        return response.status(StatusCodes.BAD_REQUEST).send(new ResponseData(StatusCodes.BAD_REQUEST, "", result));
+    } else if (result instanceof NotFoundError) {
+        return response.status(StatusCodes.NOT_FOUND).send(new ResponseData(StatusCodes.NOT_FOUND, "", result));
+
+    } else if (result instanceof InternalServerError) {
+        return response.status(StatusCodes.INTERNAL_SERVER_ERROR).send(new ResponseData(StatusCodes.INTERNAL_SERVER_ERROR, "", result));
+    }
+    return response.status(statusCode).send(responseData);
 }
--- src/service/Orders/OrdersService.ts
+++ src/service/Orders/OrdersService.ts
@@ -1,6 +1,4 @@
 import {getRepository} from "typeorm";
-import {ResponseData} from "../../controller/ResponseData";
-import {StatusCodes} from "http-status-codes";
 import {Orders} from "../../domain/Orders/Orders";
 import { v4 as uuid } from 'uuid';
 import {OrderHistoryDTO} from "../../controller/Orders/OrderHistoryDTO";
@@ -10,154 +8,170 @@ import {OrdersRepository} from "../../domain/Orders/OrdersRepository";
 import {UserRepository} from "../../domain/Users/UserRepository";
 import {Role} from "../Users/UsersService";
 import {Users} from "../../domain/Users/Users";
+import NotFoundError from "../../utils/errors/NotFoundError";
+import BadRequestError from "../../utils/errors/BadRequestError";
+import InternalServerError from "../../utils/errors/InternalServerError";
+import logger from "../../utils/Logger";
 
 export class OrdersService {
 
     repository = getRepository(Orders);
 
-    async count():(Promise<ResponseData>) {
-        return new ResponseData(StatusCodes.OK, "", {
-            orders_count: await OrdersRepository.getInstance().count()
-        });
+    async count(): Promise<Number> {
+        return await OrdersRepository.getInstance().count();
     }
 
-    async list(skip:number, limit:number, userId: number):(Promise<ResponseData>) {
-        const user = await UserRepository.getInstance().findById(userId);
+    async list(skip:number, limit:number, userId: number): Promise<OrderDTO[]> {
+        const user = await getUserById(userId);
         const list: Orders[] = await OrdersRepository.getInstance().find(skip, limit);
         const orders = filterOrdersByUser(list, user);
-        return new ResponseData(StatusCodes.OK, "",  await Promise.all(orders.map(async (order) => getCompleteOrder(order))));
+        return await Promise.all(orders.map(async (order) => getCompleteOrder(order)));
     }
 
-    async listByDateRange(startDate: Date, endDate: Date, skip:number, limit:number, userId: number):(Promise<ResponseData>) {
-        const user = await UserRepository.getInstance().findById(userId);
+    async listByDateRange(startDate: Date, endDate: Date, skip:number, limit:number, userId: number): Promise<OrderDTO[] | InternalServerError> {
+        const user = await getUserById(userId);
         const list = await OrdersRepository.getInstance().findByDateRange(startDate, endDate, skip, limit);
         const orders = filterOrdersByUser(list, user);
-        return new ResponseData(StatusCodes.OK, "",  await Promise.all(orders.map(async (order) => getCompleteOrder(order))));
+        return await Promise.all(orders.map(async (order) => getCompleteOrder(order)));
     }
 
 
-    async listByStatus(orderStatus: string, skip:number, limit:number, userId: number):(Promise<ResponseData>) {
-        const user = await UserRepository.getInstance().findById(userId);
+    async listByStatus(orderStatus: string, skip:number, limit:number, userId: number): Promise<OrderDTO[] | InternalServerError> {
+        const user = await getUserById(userId);
         const list = await OrdersRepository.getInstance().findByStatus(orderStatus, skip, limit);
         const orders = filterOrdersByUser(list, user);
-        return new ResponseData(StatusCodes.OK, "", await Promise.all(orders.map(async (order) => getCompleteOrder(order))));
+        return await Promise.all(orders.map(async (order) => getCompleteOrder(order)));
     }
 
-    async get(id: uuid, userId: number):(Promise<ResponseData>) {
-        const user = await UserRepository.getInstance().findById(userId);
-        const order = await OrdersRepository.getInstance().findById(id);
+    async listOrderItemsByOrderId(orderId: uuid, skip: number, limit: number, userId: number): Promise<OrderItemDTO[] | NotFoundError> {
+        const user = await getUserById(userId);
+        const order = await OrdersRepository.getInstance().findById(orderId);
         if (!order) {
-            return new ResponseData(StatusCodes.NOT_FOUND, "Nenhum pedido encontrado!");
+            return new NotFoundError(`Nenhum pedido encontrado com o ID ${orderId}!`);
 
         } else if (user.role !== Role.ADMIN && order.user.id !== user.id) {
-            return new ResponseData(StatusCodes.NOT_FOUND, "Nenhum pedido encontrado com o ID informado!");
+            return new NotFoundError(`Nenhum pedido encontrado com o ID ${orderId}!`)
         }
 
-        return new ResponseData(StatusCodes.OK, "", await getCompleteOrder(order));
+        return OrderItemDTO.mapToListDTO(await OrdersRepository.getInstance().findOrderItemsByOrderId(orderId, skip, limit));
     }
 
-    async create(user_id: number, orderItems: OrderItemRequest[]):(Promise<ResponseData>) {
-        let order = await OrdersRepository.getInstance().createOrder(user_id);
-        return new ResponseData(StatusCodes.CREATED, "", await createItemsAndGetOrder(orderItems, order));
-    }
-
-    async addRemoveOrderItems(order_id: uuid, orderItems: OrderItemRequest[]):(Promise<ResponseData>) {
-        const order = await OrdersRepository.getInstance().findById(order_id);
+    async listOrderHistoryByOrderId(orderId: uuid, skip: number, limit: number, userId: number): Promise<OrderHistoryDTO[] | NotFoundError> {
+        const user = await getUserById(userId);
+        const order = await OrdersRepository.getInstance().findById(orderId);
         if (!order) {
-            return new ResponseData(StatusCodes.NOT_FOUND, "Nenhum pedido encontrado com o ID informado!");
-
-        } else if (order.orderStatus !== OrderStatus.NEW) {
-            return new ResponseData(StatusCodes.BAD_REQUEST, "O Pedido informado não pode ser modificado!");
-        }
-
-        // delete all existing items
-        await OrdersRepository.getInstance().deleteOrderItemsByOrderId(order.id);
-
-        return new ResponseData(StatusCodes.OK, "", await createItemsAndGetOrder(orderItems, order));
-    }
-
-    async listOrderItemsByOrderId(order_id: uuid, skip: number, limit: number): Promise<ResponseData> {
-        const order = await OrdersRepository.getInstance().findById(order_id);
-        if (!order) {
-            return new ResponseData(StatusCodes.NOT_FOUND, "Nenhum pedido encontrado com o ID informado!");
-        }
-        return new ResponseData(StatusCodes.OK, "",
-                    OrderItemDTO.mapToListDTO(await OrdersRepository.getInstance().findOrderItemsByOrderId(order_id, skip, limit)))
-    }
-
-    async listOrderHistoryByOrderId(order_id: uuid, skip: number, limit: number, userId: number): Promise<ResponseData> {
-        const user = await UserRepository.getInstance().findById(userId);
-        const order = await OrdersRepository.getInstance().findById(order_id);
-        if (!order) {
-            return new ResponseData(StatusCodes.NOT_FOUND, "Nenhum pedido encontrado com o ID informado!");
+            return new NotFoundError(`Nenhum pedido encontrado com o ID ${orderId}!`)
 
         } else if (user.role !== Role.ADMIN && order.user.id !== user.id) {
-            return new ResponseData(StatusCodes.NOT_FOUND, "Nenhum pedido encontrado com o ID informado!");
+            return new NotFoundError(`Nenhum pedido encontrado com o ID ${orderId}!`)
         }
 
-        return new ResponseData(StatusCodes.OK, "",
-            OrderHistoryDTO.mapToListDTO(await OrdersRepository.getInstance().findOrdersHistoryByOrderId(order_id, skip, limit)))
+        return OrderHistoryDTO.mapToListDTO(await OrdersRepository.getInstance().findOrdersHistoryByOrderId(orderId, skip, limit));
     }
 
-    async deleteOrderAndItems(id: uuid):(Promise<ResponseData>) {
-        const order = await OrdersRepository.getInstance().findById(id);
+    async get(orderId: uuid, userId: number): Promise<OrderDTO | NotFoundError> {
+        const user = await getUserById(userId);
+        const order = await OrdersRepository.getInstance().findById(orderId);
         if (!order) {
-            return new ResponseData(StatusCodes.NOT_FOUND, "Nenhum pedido encontrado!");
+            return new NotFoundError(`Nenhum pedido encontrado com o ID ${orderId}!`)
+
+        } else if (user.role !== Role.ADMIN && order.user.id !== user.id) {
+            return new NotFoundError(`Nenhum pedido encontrado com o ID ${orderId}!`)
+        }
+
+        return await getCompleteOrder(order);
+    }
+
+    async create(userId: number, orderItems: OrderItemRequest[]): Promise<OrderDTO | InternalServerError> {
+        let order = await OrdersRepository.getInstance().createOrder(userId);
+        return await createItemsAndGetOrder(orderItems, order);
+    }
+
+    async addRemoveOrderItems(orderId: uuid, orderItems: OrderItemRequest[]): Promise<OrderDTO | NotFoundError | BadRequestError | InternalServerError> {
+        const order = await OrdersRepository.getInstance().findById(orderId);
+        if (!order) {
+            return new NotFoundError(`Nenhum pedido encontrado com o ID ${orderId}!`)
 
         } else if (order.orderStatus !== OrderStatus.NEW) {
-            return new ResponseData(StatusCodes.BAD_REQUEST, "O Pedido informado não pode ser excluido!");
+            return new BadRequestError("O Pedido informado não pode ser modificado!")
         }
 
-        // delete all items first
-        await OrdersRepository.getInstance().deleteOrderItemsByOrderId(order.id);
+        try {
+            // delete all existing items
+            await OrdersRepository.getInstance().deleteOrderItemsByOrderId(order.id);
 
-        // delete should not be necessary since first history entry is created only after first status update
-        await OrdersRepository.getInstance().deleteOrdersHistoryByOrderId(order.id);
+            return await createItemsAndGetOrder(orderItems, order);
+        } catch (error) {
+            logger.error("Error while deleting items", error);
+            return new InternalServerError("Ocorreu um erro ao remover os items do pedido")
+        }
 
-        // now delete de order
-        await OrdersRepository.getInstance().deleteOrderById(order.id);
-
-        return new ResponseData(StatusCodes.NO_CONTENT, "Pedido removido com sucesso!");
     }
 
-    async updateOrderStatus(id: uuid, orderStatus: string, changeReason: string, userId: number):(Promise<ResponseData>) {
-        const user = await UserRepository.getInstance().findById(userId);
-        const order = await OrdersRepository.getInstance().findById(id);
+    async deleteOrderAndItems(orderId: uuid): Promise<void | NotFoundError | BadRequestError | InternalServerError> {
+        const order = await OrdersRepository.getInstance().findById(orderId);
         if (!order) {
-            return new ResponseData(StatusCodes.NOT_FOUND, "Nenhum pedido encontrado!");
+            return new NotFoundError(`Nenhum pedido encontrado com o ID ${orderId}!`)
+
+        } else if (order.orderStatus !== OrderStatus.NEW) {
+            return new BadRequestError("O Pedido informado não pode ser modificado!")
+        }
+        try {
+            // delete all items first
+            await OrdersRepository.getInstance().deleteOrderItemsByOrderId(order.id);
+
+            // delete should not be necessary since first history entry is created only after first status update
+            await OrdersRepository.getInstance().deleteOrdersHistoryByOrderId(order.id);
+
+            // now delete de order
+            await OrdersRepository.getInstance().deleteOrderById(order.id);
+        } catch (error) {
+            logger.error("Error while deleting order/items/history", error);
+            return new InternalServerError("Ocorreu um erro ao remover o pedido ou os items ou o histórico")
         }
 
+    }
+
+    async updateOrderStatus(orderId: uuid, orderStatus: string, changeReason: string, userId: number): Promise<OrderDTO | NotFoundError | BadRequestError | InternalServerError> {
+        const user = await getUserById(userId);
+        const order = await OrdersRepository.getInstance().findById(orderId);
+        if (!order) {
+            return new NotFoundError(`Nenhum pedido encontrado com o ID ${orderId}!`)
+        }
         // OrderStatus always "move" forward, never backwards
         if (Number(order.orderStatus) > Number(orderStatus)) {
-            return new ResponseData(StatusCodes.BAD_REQUEST, "O novo status de pedido informado não é válido!");
+            return new BadRequestError("O novo status de pedido informado não é válido!")
         }
-
         if (user.role !== Role.ADMIN) {
             if (orderStatus !== OrderStatus.CREATED && orderStatus !== OrderStatus.CANCELED) {
-                return new ResponseData(StatusCodes.BAD_REQUEST, "O novo status de pedido informado não é válido!");
+                return new BadRequestError("O novo status de pedido informado não é válido!")
             }
         }
+        try {
+            // save status before change to new one
+            const previous = order.orderStatus;
+            const orderDate = previous === OrderStatus.NEW? new Date(): order.orderDate;
 
-        // save status before change to new one
-        const previous = order.orderStatus;
-        const orderDate = previous === OrderStatus.NEW? new Date(): order.orderDate;
+            await OrdersRepository.getInstance().updateOrderStatus(order, orderDate, orderStatus);
 
-        await OrdersRepository.getInstance().updateOrderStatus(order, orderDate, orderStatus);
+            // add the change to the OrdersHistory
+            await OrdersRepository.getInstance().createOrdersHistory(order, {
+                order_id: order.id,
+                previousStatus: previous,
+                currentStatus: order.orderStatus,
+                changeReason: changeReason
+            });
 
-        // add the change to the OrdersHistory
-        await OrdersRepository.getInstance().createOrdersHistory(order, {
-            order_id: order.id,
-            previousStatus: previous,
-            currentStatus: order.orderStatus,
-            changeReason: changeReason
-        });
-
-        return new ResponseData(StatusCodes.OK, "Situação do Pedido atualizada com sucesso!", getCompleteOrder(order));
+            return getCompleteOrder(order);
+        } catch (error) {
+            logger.error("Error while updating order", error);
+            return new InternalServerError("Ocorreu um erro ao atualizar o status do pedido")
+        }
     }
 
 }
 
-function filterOrdersByUser(list: Orders[], user: Users) {
+function filterOrdersByUser(list: Orders[], user: Users): Orders[] {
     return list.filter(order => {
         if (user.role === Role.ADMIN) {
             return order;
@@ -167,19 +181,34 @@ function filterOrdersByUser(list: Orders[], user: Users) {
     });
 }
 
-// add items to an provided order
-async function createItemsAndGetOrder(orderItems: OrderItemRequest[], order: Orders): Promise<OrderDTO> {
-    await OrdersRepository.getInstance().createOrderItems(orderItems, order);
-    return await getCompleteOrder(order);
+async function getUserById(userId: number): Promise<Users> {
+    return await UserRepository.getInstance().findById(userId);
+}
+
+// add items to a provided order
+async function createItemsAndGetOrder(orderItems: OrderItemRequest[], order: Orders): Promise<OrderDTO | InternalServerError> {
+    try {
+        await OrdersRepository.getInstance().createOrderItems(orderItems, order);
+        return await getCompleteOrder(order);
+    } catch (error) {
+        logger.error("Error while creating order items", error);
+        return new InternalServerError("Ocorreu um erro ao criar os items do pedido");
+    }
+
 }
 
 // retrieves an order with all items and history
-async function getCompleteOrder(order: Orders):Promise<OrderDTO> {
+async function getCompleteOrder(order: Orders): Promise<OrderDTO> {
 
-    const orderItems = await OrdersRepository.getInstance().findOrderItemsByOrderId(order.id, 0, 0);
-    const orderHistory = await OrdersRepository.getInstance().findOrdersHistoryByOrderId(order.id, 0, 0);
+    try {
+        const orderItems = await OrdersRepository.getInstance().findOrderItemsByOrderId(order.id, 0, 0);
+        const orderHistory = await OrdersRepository.getInstance().findOrdersHistoryByOrderId(order.id, 0, 0);
 
-    return OrderDTO.mapToDTO(order,OrderItemDTO.mapToListDTO(orderItems), OrderHistoryDTO.mapToListDTO(orderHistory));
+        return OrderDTO.mapToDTO(order,OrderItemDTO.mapToListDTO(orderItems), OrderHistoryDTO.mapToListDTO(orderHistory));
+    } catch (error) {
+        logger.error("Error while fetching complete order", error);
+        throw new InternalServerError("Ocorreu um erro ao buscar o pedido completo");
+    }
 }
 
 export enum OrderStatus { NEW = "0", CREATED = "1", IN_PROGRESS = "2", FINISHED = "3", CANCELED = "9" }
