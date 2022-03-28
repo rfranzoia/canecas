d876cbb721afb1760f47fa5d5a0b64ef3bdff906
Romeu Franzoia Junior <rfranzoia@gmail.com>
2022-03-28 11:06:42 +0100

    Fixed ProductTypesController.test.ts.test and all dependencies to match new project structure

--- src/__test__/ProductTypesController.test.ts
+++ src/__test__/ProductTypesController.test.ts
@@ -3,20 +3,22 @@ import app from "../api/api";
 import {StatusCodes} from "http-status-codes";
 import {TestHelper} from "./TestHelper";
 import {Role} from "../domain/Users/Users";
+import {mongoConnect, mongoDisconnect} from "../database/mongo";
 
 describe("ProductTypes API test (requires jwt token for most)", () => {
     let loggedUser;
 
     const TEST_PRODUCT_TYPE = {
-        description: "Some dummy description for a test productTypes that needs it",
-        image: "imagePath/imageName.png",
+        description: "Test Type",
     };
 
     beforeAll(async () => {
+        await mongoConnect();
         loggedUser = await TestHelper.createLoginUserAndAuthenticate(Role.ADMIN);
     });
 
     afterAll(async () => {
+        await mongoDisconnect();
         await TestHelper.deleteLoginTestUser(loggedUser._id);
     });
 
@@ -25,12 +27,12 @@ describe("ProductTypes API test (requires jwt token for most)", () => {
             const response = await supertest(app)
                 .get("/api/productTypes")
             expect(response.statusCode).toBe(StatusCodes.OK);
-            expect(response.body.data.length).toBeGreaterThan(0);
+            expect(response.body.length).toBeGreaterThan(0);
         });
 
     });
 
-    describe("given a product doesn't exists in the database", () => {
+    describe("given a product type doesn't exists in the database", () => {
         let createdProductType;
 
         it("should be able to add the new product", async () => {
@@ -39,12 +41,12 @@ describe("ProductTypes API test (requires jwt token for most)", () => {
                 .set("Authorization", "Bearer " + loggedUser.authToken)
                 .send(TEST_PRODUCT_TYPE);
         expect(response.statusCode).toBe(StatusCodes.CREATED);
-        createdProductType = response.body.data;
+        createdProductType = response.body;
         });
 
         it("and should be able to delete the recently created product", async () => {
             const response = await supertest(app)
-                .delete(`/api/productTypes/${createdProductType.id}`)
+                .delete(`/api/productTypes/${createdProductType._id}`)
                 .set("Authorization", "Bearer " + loggedUser.authToken);
             expect(response.statusCode).toBe(StatusCodes.NO_CONTENT);
         });
