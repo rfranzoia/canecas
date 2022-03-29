91b87e367130cb7811acfa8f3d79c21df30ede02
Romeu Franzoia Junior <rfranzoia@gmail.com>
2022-03-29 21:04:33 +0100

    changed cors configuration added Swagger API documentation to the project

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,18 +2,36 @@ import helmet from "helmet";
 import cors from "cors";
 import morgan from 'morgan';
 import express, {Router} from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
+import {TokenService} from "../security/TokenService";
 import productTypeRouter from "./ProductTypeRouter";
 import productRouter from "./ProductsRouter";
 import usersRouter from "./UsersRouter";
 import ordersRouter from "./OrdersRouter";
-import {TokenService} from "../security/TokenService";
 
 const app = express();
 
 app.use(helmet());
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+    origin: "*",
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
+
 app.use(morgan('[:method] - :date[iso] ":url" :status :response-time ms - :res[content-length]'));
+
+const options = {
+    customCss: '.swagger-ui .topbar { display: none }',
+    customSiteTitle: "Canecas Service"
+};
+
+app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument, options)
+);
 
 app.get('/', (req, res) => {
     res.send({
