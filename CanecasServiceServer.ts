011cf6bc503e9fa6e57e06f44fc00d739034a77e
Romeu Franzoia Junior <rfranzoia@gmail.com>
2022-04-17 22:11:40 +0100

    moved configuration from Api class for publication and frontend integration

--- CanecasServiceServer.ts
+++ CanecasServiceServer.ts
@@ -5,12 +5,19 @@ import express from "express";
 import path from "path";
 
 const PORT = process.env.SERVER_PORT || 3500;
-export const imagesPath = __dirname + "/public/images/";
+export const imagesPath = __dirname + "/images/";
 
 const start = async () => {
   await mongoConnect();
 
-  app.use("/api", express.static(path.join(__dirname, "public")));
+  // setup client path
+  app.use("/api", express.static(__dirname));
+  app.use(express.static(__dirname + "/public/"));
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
+
   app.listen(PORT, () => {
     return logger.info(`Express is listening at http://localhost:${PORT}`);
   });
